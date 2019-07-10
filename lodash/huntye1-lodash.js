b409a4c68ee33ea933d2761730c648a278eb403d
huntye1 <wm0548@qq.com>
2019-07-10 21:40:34 +0800

    chunck

--- lodash/huntye1-lodash.js
+++ lodash/huntye1-lodash.js
@@ -1,5 +1,26 @@
 var huntye1 = {
-  compact: function (arr) { 
+  compact: function compact (arr) { 
     return arr.filter(item => item);
+  },
+
+  chunck: function chunck (arr,size = 1) { 
+    let res = [];
+    for (let i = 0; i < arr.length; ) { 
+      if (i + size - 1 < arr.length && size > 1) {
+        let temp = [];
+        let count = size;
+        while (count--) {
+          temp.push(arr[i]);
+          i++;
+        }
+        res.push(temp)
+      } else { 
+        res.push(arr[i]);
+        i++
+      }
+    }
+    return res;
   }
+
+
 }
